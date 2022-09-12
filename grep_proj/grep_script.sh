9b90a2ad3afbdf1d0b21dfe121a5b3898f4cc4b6
Luca Mikulastik <mikulastikc@gmail.com>
2022-09-12 18:11:48 +0000

    added functionality for first four rules

--- grep_proj/grep_script.sh
+++ grep_proj/grep_script.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# words ending in ing
+grep "/*ing" ./grep_wordlist.txt | sort 
+
+# number of words ening ing
+grep -c "/*ing" ./grep_wordlist.txt | sort
+
+# words starting in be
+grep "be/*" ./grep_wordlist.txt | sort
+
+# exact word test
+grep -w "test" ./grep_wordlist.txt | sort
+
+
+
