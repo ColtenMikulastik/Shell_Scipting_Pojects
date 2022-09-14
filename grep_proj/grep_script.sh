5bbafeb6c274300294b570c35ffbd349f672589a
Colten <mikulastikc@gmail.com>
2022-09-13 20:57:22 -0500

    fixed a couple of thing, I wanna pull my hair out

--- grep_proj/grep_script.sh
+++ grep_proj/grep_script.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 # words ending in ing
-grep "/*ing" ./grep_wordlist.txt | sort 
+grep .*ing ./grep_wordlist.txt | sort 
 
 # number of words ening ing
-grep -c "/*ing" ./grep_wordlist.txt | sort
+grep -c .*ing ./grep_wordlist.txt | sort
 
 # words starting in be
-grep "be/*" ./grep_wordlist.txt | sort
+grep ^be.* ./grep_wordlist.txt | sort
 
 # exact word test (I don't know why this isn't working
 grep -w "test" ./grep_wordlist.txt | sort
 
 # word starts with m ends with t
-grep "m/*t" ./grep_wordlist.txt | sort
+grep m.*t ./grep_wordlist.txt | sort
 
+# MAC addresses
+grep ..:..:..:..:..:.. ./grep_wordlist.txt | sort
 
+# telephone addresses isn't working
+grep \(...\)-...-.... ./grep_wordlist.txt | sort
+
+# email addresses 
+grep .*\@.*\.com ./grep_wordlist.txt | sort
+
+# social security numbers
+
+# IP address
 
