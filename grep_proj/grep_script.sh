24c37424ce0652431a4784c7e149c73a9e1073ab
Luca Mikulastik <mikulastikc@gmail.com>
2022-09-17 10:51:34 +0000

    added more echo cmd explaining whats happening

--- grep_proj/grep_script.sh
+++ grep_proj/grep_script.sh
@@ -13,43 +13,62 @@ spec_grep () {
 
 
 # words ending in ing
-echo "looking for words ending in \"ing\":..."
+echo "<starting>: looking for words ending in \"ing\":..."
 spec_grep .*ing
+echo "<finishing>: looking for words ending in \"ing\":..."
+echo ""
 
 # number of words ening ing
-echo "counting words ending in \"ing\":..."
+echo "<starting>: counting words ending in \"ing\":..."
 spec_grep -c .*ing
+echo "<finishing>: counting words ending in \"ing\":..."
+echo ""
 
 # words starting in be
-echo "looking for words starting with \"be\":..."
+echo "<starting>: looking for words starting with \"be\":..."
 spec_grep ^be.*
+echo "<finishing>: looking for words starting with \"be\":..."
+echo ""
 
 # exact word test (I don't know why this isn't working)
-echo "looking for exact word \"test\":..."
+echo "<starting>: looking for exact word \"test\":..."
 spec_grep -w test 
+echo "<finishing>: looking for exact word \"test\":..."
+echo ""
 
 # word starts with m ends with t
-echo "looking for word that starts with \"m\" and ends with \"t\":..."
+echo "<starting>: looking for word that starts with \"m\" and ends with \"t\":..."
 spec_grep m.*t
+echo "<finishing>: looking for word that starts with \"m\" and ends with \"t\":..."
+echo ""
 
 # MAC addresses
-echo "looking for MAC addresses:..."
+echo "<starting>: looking for MAC addresses:..."
 spec_grep ..:..:..:..:..:..
+echo "<finishing>: looking for MAC addresses:..."
+echo ""
 
 # telephone addresses
-echo "looking for telephone numbers:..."
+echo "<starting>: looking for telephone numbers:..."
 spec_grep \(...\)-...-....
+echo "<finishing>: looking for telephone numbers:..."
+echo ""
 
 # email addresses use E to use the parenthesis
-echo "looking for email addresses:..."
+echo "<starting>: looking for email addresses:..."
 spec_grep -E ".*\@.*\.(com|net)"
+echo "<finishing>: looking for email addresses:..."
+echo ""
 
 # social security numbers
-echo "looking for social security numbers:..."
+echo "<starting>: looking for social security numbers:..."
 spec_grep ...-..-....
+echo "<finishing>: looking for social security numbers:..."
+echo ""
 
 # IP address
-echo "looking for IP addresses:..."
+echo "<starting>: looking for IP addresses:..."
 spec_grep ".*\..*\..*\..*"
+echo "<finishing>: looking for IP addresses:..."
 
 
