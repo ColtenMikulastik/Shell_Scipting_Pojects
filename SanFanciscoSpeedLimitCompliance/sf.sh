76b5ce7a11bb09d38ca50639b4f46b736f815cbe
Colten <mikulastikc@gmail.com>
2022-11-10 13:15:51 -0600

    added documentation to the sf.sh file

--- SanFanciscoSpeedLimitCompliance/sf.sh
+++ SanFanciscoSpeedLimitCompliance/sf.sh
@@ -15,32 +15,28 @@ echo "About to parse from file: $1"
 # this can be done
 # cat $1 | sed 's/,/\t\t/g' > ouput.txt 
 
-
+# create a variable with the output of the input file
 file=$(cat $1)
 
+# create the ouput folder
 touch output.txt 
 
+# clear out the output folder in case it already existed
 echo "" > output.txt
 
+# loop through the file's output
 for line in $file
 do
+    # if the file is the first line so the spacing is correct
 	if [[ $line == $(head -1 $1) ]]
 	then
-		echo "TOP OF FILE!!!!!!!"
-		echo $line | sed 's/,/\t/g'
+        # the first line is only one tab
 		echo $line | sed 's/,/\t/g' >> output.txt
+        # deliniate the headers with the data
 		echo "====================================================================================================================" >> output.txt
 	else
-		echo $line | sed 's/,/\t\t/g'
+        # if the line is data, then three tab seperation
 		echo $line | sed 's/,/\t\t\t/g' >> output.txt 
 	fi
 done
 
-
-
-
-# Then, appropriate comments for whatever this script does.
-
- 
-
-# Exit status
