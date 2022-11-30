3d3637813334f350c82f7c61eb978ee4fd947505
Colten <mikulastikc@gmail.com>
2022-11-30 13:11:53 -0600

    Removed needless variable and added some status codes
    I don't know if the exit status codes work, can't run script on current machine.

--- Lab_15/change_user_password.sh
+++ Lab_15/change_user_password.sh
@@ -10,18 +10,14 @@
 # First, if the user needs to be root, check to make sure the root user is executing this command. If not, then inform the user they must use root privileges
 if [ $EUID == 0 ]
 then
-    vari=0
     for line in $(cat input.txt)
     do
 	./change_user_password.exp $line
+    echo "status: $?"
     done
 else
     echo "you are not the root user or sudoer"
     echo "please run program as sudoer"
-fi 
-
-# Then, appropriate comments for whatever this script does.
-
- 
-
-# Exit status
+    exit status 1
+fi
+exit status 0
