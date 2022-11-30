64d636d37c05dee35ef727daddc06171ee59c178
Colten <mikulastikc@gmail.com>
2022-11-29 20:19:12 -0600

    working on lab 15

---
+++ Lab_15/change_user_password.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Name: Colten Vance Mikulastik
+# Date: 11/29/2022
+# Class: Advanced Linux
+# Lab: Lab 15 Expect 
+
+# This script changes the passwords of 5 users given from teh input file
+ 
+# First, if the user needs to be root, check to make sure the root user is executing this command. If not, then inform the user they must use root privileges
+if [ $EUID == 0 ]
+then
+    vari=0
+    for line in $(cat input.txt)
+    do
+        vari=$((vari+1))
+        echo "$vari"
+        echo "$line"
+    done
+else
+    echo "you are not the root user or sudoer"
+    echo "please run program as sudoer"
+fi 
+
+# Then, appropriate comments for whatever this script does.
+
+ 
+
+# Exit status
