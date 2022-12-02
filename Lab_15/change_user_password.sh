8651198b985b5b2a47c2448a5ea99e37f1be1c03
Colten <mikulastikc@gmail.com>
2022-12-01 21:10:36 -0600

    fixed many problems with the script and removed statuses

--- Lab_15/change_user_password.sh
+++ Lab_15/change_user_password.sh
@@ -10,14 +10,14 @@
 # First, if the user needs to be root, check to make sure the root user is executing this command. If not, then inform the user they must use root privileges
 if [ $EUID == 0 ]
 then
+    # loop through the users in the input.txt file
     for line in $(cat input.txt)
     do
-	./change_user_password.exp $line
-    echo "status: $?"
+        # call the expect script, and pass the username as the argument
+	    ./change_user_password.exp $line
     done
 else
+    # tell the user that they need to change their permissions
     echo "you are not the root user or sudoer"
     echo "please run program as sudoer"
-    exit status 1
 fi
-exit status 0
