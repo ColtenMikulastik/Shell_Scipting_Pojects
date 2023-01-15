00d68464f05198dc0cf7a0ff616b3ee42d0eff9f
Colten <mikulastikc@gmail.com>
2023-01-14 22:34:31 -0600

    now will read information from the input file, and find distros accordingly

--- Distro_Watcher/webscrape.py
+++ Distro_Watcher/webscrape.py
@@ -2,19 +2,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+def read_inputfile(distros):
+    with open("input.txt", 'r') as f:
+        for line in f.readlines():
+            distros.append(line.strip())
+
 page = requests.get("https://distrowatch.com/dwres.php?resource=bittorrent")
 soup = BeautifulSoup(page.content, "html.parser")
+
+distros = []
+read_inputfile(distros)
+print(distros)
 
 save_next_link = False
 for torrent in soup.find_all("table")[6].find_all("tr"):
 
     for name_link in torrent.find_all("td", class_="torrent"):
-        if name_link.get_text() == "Manjaro Linux":
-            print("found a sparky: " + name_link.get_text())
-            save_next_link = True
-        elif save_next_link == True:
-            print("this is sparky link: " + name_link.get_text())
-            save_next_link = False
+        for distro in distros:
+            if name_link.get_text() == distro:
+                print("found a sparky: " + name_link.get_text())
+                save_next_link = True
+            elif save_next_link == True:
+                print("this is sparky link: " + name_link.get_text())
+                save_next_link = False
 
 
 
