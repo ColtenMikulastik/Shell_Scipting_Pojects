466ddb3b049d31914cf8464c82edfc920ec629eb
Colten <mikulastikc@gmail.com>
2023-01-15 14:07:42 -0600

    working on adding dictionary with list of torrent links for a distro

--- Distro_Watcher/README.md
+++ Distro_Watcher/README.md
@@ -1,3 +1,4 @@
 # Distro_Watcher
 
 This program will download new updates of linux distro's that you tell it to. Using the "distrowatch.com" Torrent Archive to know when to download a new update.
+
--- Distro_Watcher/webscrape.py
+++ Distro_Watcher/webscrape.py
@@ -7,6 +7,7 @@ def read_inputfile(distros):
         for line in f.readlines():
             distros.append(line.strip())
 
+
 page = requests.get("https://distrowatch.com/dwres.php?resource=bittorrent")
 soup = BeautifulSoup(page.content, "html.parser")
 
@@ -14,18 +15,27 @@ distros = []
 read_inputfile(distros)
 print(distros)
 
+dict_distros = {}
+
+
 save_next_link = False
 for torrent in soup.find_all("table")[6].find_all("tr"):
 
     for name_link in torrent.find_all("td", class_="torrent"):
         for distro in distros:
+            all_relivant_links = []
             if name_link.get_text() == distro:
                 print("found a sparky: " + name_link.get_text())
                 save_next_link = True
             elif save_next_link == True:
                 print("this is sparky link: " + name_link.get_text())
                 save_next_link = False
+                all_relivant_links.append(name_link.get_text())
+            print(all_relivant_links)
+            dict_distros[distro] = all_relivant_links
 
+
+print(dict_distros)
 
 
 #for torrent in soup.find_all("table")[6].find_all("td", class_="torrent"):
