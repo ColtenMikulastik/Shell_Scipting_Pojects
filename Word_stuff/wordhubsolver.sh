1faa89f364d9b88e05bfce3a6f874544946815b1
ColtenMikulastik <mikulastikc@gmail.com>
2023-02-27 05:17:22 +0600

    working on it

--- Word_stuff/wordhubsolver.sh
+++ Word_stuff/wordhubsolver.sh
@@ -10,8 +10,12 @@ echo "HEY $USER WAZAH!"
 echo "please give a word: "
 read info
 
-
+# this will loop through the characters in the string givin
 for (( i = 0; i<${#info}; i++ ))
 do
 	echo "${info:$i:1}"
+
 done
+
+
+grep $info words.txt
